bee83079535c54b43ed0e9861b9c7a39eacc4144
kebohonganyangjujur <kebohonganyangjujur@gmail.com>
2022-06-14 03:18:34 +0700

    complete IBAction

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player : AVAudioPlayer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
 
+        do {
+            
+            let player = try AVAudioPlayer(contentsOf: url)
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        print("Button pressed")
+        print(sender.currentTitle)
+    }
+    
     
 
 }
